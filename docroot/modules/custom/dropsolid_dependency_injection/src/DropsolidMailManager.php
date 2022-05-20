61f4caf8e94e9ec7f9b97a9ad4c507613b220bbb
devbranch-vitaliy <v.podoba@dev-branch.com>
2022-05-20 11:29:43 +0300

    Updated a comment in the doMail() function - added my explanations and suggetions

--- docroot/modules/custom/dropsolid_dependency_injection/src/DropsolidMailManager.php
+++ docroot/modules/custom/dropsolid_dependency_injection/src/DropsolidMailManager.php
@@ -17,10 +17,14 @@ class DropsolidMailManager extends MailManager {
   public function doMail($module, $key, $to, $langcode, $params = [], $reply = NULL, $send = TRUE) {
     // Always send mails to a certain e-mail address.
     //
-    // Notice: This is an example of how to replace a standard service with
+    // P.S: This is an example of how to replace a standard service with
     // a custom one. But it's better to use such a solution if we want
-    // to implement major changes. For a similar small fix it is better to use
-    // hook_mail() or hook_mail_alter().
+    // to implement major changes of the service. For a similar small custom
+    // fix it is better to use hook_mail() or hook_mail_alter().
+    //
+    // P.S.S. But a much better solution would be to use some contrib module.
+    // E.g. reroute_email.
+    // @see https://www.drupal.org/project/reroute_email
     $to = 'nope@doesntexist.com';
     return parent::doMail($module, $key, $to, $langcode, $params, $reply, $send);
   }
